71d28dcc04f2820dea7be5fc9d03bb1ea92eadd4
jdagudelo10 <juan_dav.agudelo@uao.edu.co>
2026-02-11 15:22:20 -0500

    challenge 1 de arrow functions

---
+++ CHALLENGE_01/arrowFunctions.js
@@ -0,0 +1,7 @@
+//EJERCICIO USANDO FUNCIONES REGULARES
+function determinarCondicion(enfermedad){
+    const nombre = "César Andrés Díaz Jimenez"
+    const enfermedad = "Homosexualidad"
+
+    return `${nombre} padece de ${enfermedad}`
+}
