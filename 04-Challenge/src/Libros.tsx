f94aa72ec07543ab9d0eb18701504d0df3a0838a
jdagudelo10 <juan_dav.agudelo@uao.edu.co>
2026-03-11 15:01:10 -0500

    .

---
+++ 04-Challenge/src/Libros.tsx
@@ -0,0 +1,4 @@
+function Libros(){
+
+    const Books = []
+}
--- 04-Challenge/src/main.tsx
+++ 04-Challenge/src/main.tsx
@@ -1,15 +1,10 @@
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import './index.css'
-import Pacientes from './Componentes/Pacientes'
-import Medico from './Componentes/Medico'
-import Comite from './Componentes/Comite'
 
 
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
-    <Pacientes/>
-    <Medico/>
-    <Comite/>
+
   </StrictMode>,
 )
